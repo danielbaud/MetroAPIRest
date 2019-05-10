3d8f5b5a0f7ac300aab1264fc6e8e4a0dcb25236
Vincent <vincent1.riviere@epita.fr>
2019-05-10 15:47:38 +0200

    [CLIENT] Client added as endpoint in api

--- src/app/status/GetClients.scala
+++ src/app/status/GetClients.scala
@@ -1,23 +1,31 @@
 package status
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import akka.util.ByteString
 import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.libs.json.Json
 import scala.io.Source
+import javax.inject.Inject
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+import scala.concurrent.ExecutionContext
 
-class GetClients @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class GetClients @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
 
     def getClients = Action { implicit request =>
-    val source = Source.fromFile("other/clients.json")
-        var jsons = "["
+        val source = Source.fromFile("../other/clients.json")
         for (line <- source.getLines()) {
-            jsons += line
-            jsons += ","
+          var data = Json.parse(line)
+          ws.url("http://localhost:9000/status").post(data)
         }
-        jsons += "]"
         source.close()
-        Ok(jsons)
+        Ok("Success")
     }
 
 }
