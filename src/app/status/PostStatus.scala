1a49dd00572faa13a7d99404df1cce108532f63d
Vincent <vincent1.riviere@epita.fr>
2019-07-03 15:07:01 +0200

    [ARCHI] Chmod updated

--- src/app/status/PostStatus.scala
+++ src/app/status/PostStatus.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.libs.json.Json
+import org.apache.spark.{SparkContext, SparkConf}
 
 class PostStatus @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
